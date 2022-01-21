4a7fb44b4cd95e8913d9fc0058e57a509059b209
Nathan Rocher <3007nat@gmail.com>
2022-01-21 23:29:35 +0100

    + Turning up screens using virtual mouse movement

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 require('dotenv').config();
 
 const { SinricPro, SinricProActions, raiseEvent, eventNames } = require("sinricpro");
-
 const turnOffDisplay = require("turn-off-display");
+const robot = require("robotjs");
 
 const appKey = process.env.SINRIC_KEY
 const secretKey = process.env.SINRIC_SECRET
 const device1 = process.env.SINRIC_DEVICE
+
+function turnOnDisplay() {
+    robot.moveMouse(0, 0);
+    robot.moveMouse(100, 100);
+    robot.moveMouse(200, 200);
+}
+
 
 function setPowerState(deviceid, data) {
     console.log("setPowerState", data)
 
     if (data === "Off") {
         turnOffDisplay();
+    } else {
+        turnOnDisplay();
     }
 
     return true
@@ -25,7 +34,7 @@ function initiate() {
         setPowerState
     })
 
-    sinricpro.on("error", _ => {})
+    sinricpro.on("error", _ => { })
 
     sinricpro.on("close", _ => {
         console.info("[INFO] Close event")
